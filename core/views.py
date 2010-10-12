c9b54633f115a95c04dc840e6fcd57130c4b8a83
XandO <frankie.the.coin@gmail.com>
2010-10-13 00:27:42 +0200

    joke application last lines removed

--- core/views.py
+++ core/views.py
@@ -1,14 +1,13 @@
 from django.views.generic.simple import direct_to_template
 from django.shortcuts import get_object_or_404, get_list_or_404
 
-from funnybag.joke.models import *
+from funnybag.core.models import *
 
 def details(request, joke_id):
     return direct_to_template(request, 'code/details.html',
-                              {'joke': get_object_or_404(Joke, pk=joke_id) })
+                              {'record': get_object_or_404(Joke, pk=joke_id) })
 
 def list(request):
     records = Record.objects.order_by('-created_time')
     return direct_to_template(request, 'core/list.html',
                               {'records': records })
-
