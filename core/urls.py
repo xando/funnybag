0f2050e59160323723ee2a11a7118f8d15bff6f1
xando <sebastian.pawlus@gmail.com>
2010-10-22 21:28:51 +0200

    url pattern for new elements

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('core.views',
                        (r'^$', 'list'),
                        (r'^in_line/?$', 'list'),
-                       (r'^(?P<record_id>\d+)$', 'details')
+                       (r'^(?P<record_id>\d+)$', 'details'),
+                       (r'^new$', 'new')
                        )
