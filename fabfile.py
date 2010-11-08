b0d8d14efbcd4c76cd44a6b3ff7f34f580098502
xando <sebastian.pawlus@gmail.com>
2010-11-08 23:16:17 +0100

    pip managment changed to local directory

---
+++ fabfile.py
@@ -0,0 +1,47 @@
+from __future__ import with_statement
+
+import os
+from fabric.api import *
+
+from fabric.contrib.project import rsync_project
+
+env.project_name = 'project_name'
+
+def xando():
+    env.hosts = ['xando.pl']
+    env.path = '/home/services/www/funnybag'
+    env.user = 'seba'
+
+def setup():
+    run('mkdir -p %s; cd %s; virtualenv .;' % (env.path,env.path))
+
+def push():
+    "Get new development code to device"
+
+    rsync_project(remote_dir="/home/services/www/funnybag/",
+                  local_dir=".",
+                  # delete=True,
+                  exclude=[".git*",
+                           "*.pyc",
+                           "*.pyo"])
+
+    run("chown :www-data -R /home/services/www/funnybag")
+    run("chmod g+rw -R /home/services/www/funnybag/funnybag/db")
+
+
+def requirements():
+    "Install the required packages from the requirements file using pip"
+    
+    run('cd %s; pip install -E . -r requirements.txt' % env.path)
+
+def restart():
+    "Restart apache"
+
+    sudo("/etc/init.d/apache2 restart")
+
+def deploy():
+    "Full deploy: push and start"
+
+    push()
+    requirements()
+    restart()
