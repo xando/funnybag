92114f52e85ce87e0a7ce6f3d796ac14f2fca317
xando <sebastian.pawlus@gmail.com>
2010-11-09 21:37:41 +0100

    project name added to views

--- funnybag/core/urls.py
+++ funnybag/core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns
 
-urlpatterns = patterns('core.views',
+urlpatterns = patterns('funnybag.core.views',
                        (r'^$', 'list'),
                        (r'^in_line/?$', 'list'),
                        (r'^(?P<record_id>\d+)$', 'details'),
