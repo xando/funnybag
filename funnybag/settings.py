07d6f187529369a21545092380ca9eca5243d229
xando <sebastian.pawlus@gmail.com>
2010-11-11 22:44:33 +0100

    Pagination implemented with use of django-pagination. That was really easy!!

--- funnybag/settings.py
+++ funnybag/settings.py
@@ -76,7 +76,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = ("django.core.context_processors.auth",
+                               "django.core.context_processors.debug",
+                               "django.core.context_processors.i18n",
+                               "django.core.context_processors.media",
+                               "django.core.context_processors.request")
 
 ROOT_URLCONF = 'funnybag.urls'
 
@@ -96,7 +103,8 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
 
     'funnybag.core',
-    'registration'
+    'registration',
+    'pagination'
 )
 
 ACCOUNT_ACTIVATION_DAYS = 7
