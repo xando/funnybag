fec70828c3bd10ec145124e5874be23a648fd9b2
xando <sebastian.pawlus@gmail.com>
2011-04-04 00:06:00 +0200

    deploy script finished.

--- fabfile.py
+++ fabfile.py
@@ -1,45 +1,12 @@
 import os
 from fabric.api import *
 from fabric.operations import require
-# from fabric.contrib.project import rsync_project
 from fabric.contrib.files import sed
 
-
-# env.path = None
 env.project_name = 'funnybag'
 env.github_login = 'xando'
 
-
-# def xando_org():
-#     env.hosts = ['xando.org']
-#     env.path = '/var/www/%s' % env.project_name
-#     env.user = 'seba'
-
-# def setup():
-#     run('mkdir -p %s; cd %s; virtualenv .;' % (env.path,env.path))
-
-# def push():
-#     if env.path:
-#         rsync_project(remote_dir=env.path,
-#                       local_dir=".",
-#                       # delete=True,
-#                       exclude=[".git*",
-#                                "*.pyc",
-#                                "*.pyo"])
-
-#         with cd(env.path):
-#             sudo("chown :www-data -R .")
-#             sudo("chmod g+rw -R .")
-
-
-#     else:
-#         print("Deployemnt path not set")
-
-#     # "Get new development code to device"
-
-
 def setup_production(path="/var/www/%s" % env.project_name, initial_release="master"):
-    #TODO: switch to reall check. This normalization is pretty odd
     env.path = path.rstrip("/")
 
     sudo('apt-get install -y python-setuptools')
@@ -56,6 +23,15 @@ def setup_production(path="/var/www/%s" % env.project_name, initial_release="mas
     _download_release(initial_release)
     _install_requirements()
     _configure_webserver()
+    _restart()
+
+
+def update_production(path="/var/www/%s" % env.project_name, release="master"):
+    env.path = path.rstrip("/")
+
+    _download_release(release)
+    _install_requirements()
+    _restart()
 
 
 def _download_release(release):
@@ -76,12 +52,11 @@ def _install_requirements():
         del(os.environ['PIP_VIRTUALENV_BASE'])
     except KeyError:
         pass
-    sudo('pip -E %(path)s/.virtualenv install -r %(path)s/requirements.txt' % env)
+    sudo('pip -E %s/.virtualenv install -r %s/requirements.txt' % (env.path, env.path))
 
 
 def _configure_webserver():
     require("path")
-
 
     sed("%s/apache.virtualhost" % env.path,
         "PATH", env.path, use_sudo=True)
@@ -89,30 +64,11 @@ def _configure_webserver():
     sed("%s/apache.virtualhost" % env.path,
         "PROJECT_NAME", env.project_name, use_sudo=True)
 
-                             # limit='', use_sudo=False, backup='.bak')
-    # env.esc_path = env.path.replace('/','\/')
-    # require("path", "esc_path")
+    sudo("cp %s/apache.virtualhost /etc/apache2/sites-available/%s" % (env.path, env.project_name))
 
-    # sudo("sed -i 's/${PATH}/%(esc_path)s/g' %(path)s/deploy/apache.virtualhost" % env)
+    sudo("a2ensite %s" % env.project_name)
 
-    # # TODO: put here diff check between config versions
-    # sudo("cp %(path)s/deploy/apache.virtualhost /etc/apache2/sites-available/qualitio" % env)
 
-    # sudo("a2ensite qualitio")
-# def requirements():
-#     "Install the required packages from the requirements file using pip"
-
-#     run('cd %s; pip install -E . -r requirements.txt' % env.path)
-
-# def restart():
-#     "Restart apache"
-
-#     sudo("/etc/init.d/apache2 restart")
-
-# def deploy():
-#     "Full deploy: push and start"
-
-#     push()
-#     requirements()
-#     restart()
+def _restart():
+    sudo("/etc/init.d/apache2 reload")
 
