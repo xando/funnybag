4f91bd71e33afc6f2338fcfcd946f72a3608823f
xando <sebastian.pawlus@gmail.com>
2011-05-31 22:56:08 +0200

    url cleanup

--- funnybag/urls.py
+++ funnybag/urls.py
@@ -7,12 +7,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        (r'^', include('funnybag.core.urls')),
-                       (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       (r'^admin/', include(admin.site.urls)),
-                       (r'^comments/', include('django.contrib.comments.urls')),
-                       (r'^accounts/', include('registration.urls')),
+                       (r'^root/doc/', include('django.contrib.admindocs.urls')),
+                       (r'^root/', include(admin.site.urls)),
                        )
-
 
 if settings.DEBUG:
     urlpatterns += patterns('',
